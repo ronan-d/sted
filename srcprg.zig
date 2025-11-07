06ab7becbf6d48f23d723c38ba0974bfde9d9846
Ronan Desplanques <ronan.desplanques@gmail.com>
2025-11-07 20:09:32 +0100

    Change cursor strategy
    The new one is a bit of a hack because it uses a thread where a coroutine
    would suffice.

--- srcprg.zig
+++ srcprg.zig
@@ -24,7 +24,7 @@ const Expr = union(enum) {
     e_hole,
 };
 
-fn create_example_expr() !struct { root: *Expr, stack: CursorStack } {
+fn create_example_expr() !struct { root: *Expr, cursor: *ThreadCursor } {
     const e3 = try cator.create(Expr);
     e3.* = Expr{ .e_const = 3 };
 
@@ -36,22 +36,27 @@ fn create_example_expr() !struct { root: *Expr, stack: CursorStack } {
     };
 
     const ep = try cator.create(Expr);
-    const selected_op, ep.* = blk: {
+    ep.* = blk: {
         var args = try OpList.initCapacity(cator, 3);
         args.appendAssumeCapacity(Expr{ .e_const = 1 });
         args.appendAssumeCapacity(et);
         args.appendAssumeCapacity(Expr{ .e_const = 4 });
-        break :blk .{ &args.items[1], Expr{ .e_assop = .{ .o = .add, .args = args } } };
+        break :blk Expr{ .e_assop = .{ .o = .add, .args = args } };
     };
 
-    var stack: CursorStack = .{};
-    try stack.append(cator, selected_op);
+    const p = try cator.create(ThreadCursor);
+    p.* = ThreadCursor.init(ep);
 
-    return .{ .root = ep, .stack = stack };
+    p.start();
+
+    p.perform(.go_down);
+    p.perform(.go_right);
+
+    return .{ .root = ep, .cursor = p };
 }
 
-fn render_node(e: *const Expr, cursor: *Expr, parent_op: AssOp, buf: *Buffer, start: *c_int, end: *c_int) !void {
-    if (e == cursor) {
+fn render_node(e: *const Expr, cursor: usize, parent_op: AssOp, buf: *Buffer, start: *c_int, end: *c_int) !void {
+    if (@intFromPtr(e) == cursor) {
         start.* = @intCast(buf.items.len);
     }
 
@@ -87,27 +92,24 @@ fn render_node(e: *const Expr, cursor: *Expr, parent_op: AssOp, buf: *Buffer, st
         },
     }
 
-    if (e == cursor) {
+    if (@intFromPtr(e) == cursor) {
         end.* = @intCast(buf.items.len);
     }
 }
 
-const Buffer = std.ArrayList(u8);
+fn new_op(op: AssOp) !Expr {
+    var args = try OpList.initCapacity(cator, 2);
+    args.appendAssumeCapacity(Expr.e_hole);
+    args.appendAssumeCapacity(Expr.e_hole);
+    return Expr{ .e_assop = .{ .o = op, .args = args } };
+}
 
-const CursorStack = std.ArrayList(*Expr);
+const Buffer = std.ArrayList(u8);
 
 const Srcprg = struct {
     tree: *Expr,
-    cursor_stack: CursorStack,
+    cursor: *ThreadCursor,
     buf: Buffer,
-
-    fn cursor(self: @This()) *Expr {
-        if (self.cursor_stack.getLastOrNull()) |e| {
-            return e;
-        } else {
-            return self.tree;
-        }
-    }
 
     fn go_left_or_right(self: *@This(), dir: c_interface.instruction) !void {
         const ls = struct {
@@ -151,61 +153,6 @@ const Srcprg = struct {
     fn replace_cursor_with_number(self: *@This(), num: u64) void {
         overwrite_expr(self.cursor(), Expr{ .e_const = num });
     }
-
-    fn replace_cursor_with_op(self: @This(), op: AssOp) !void {
-        var args = try OpList.initCapacity(cator, 2);
-        args.appendAssumeCapacity(Expr.e_hole);
-        args.appendAssumeCapacity(Expr.e_hole);
-        overwrite_expr(self.cursor(), Expr{ .e_assop = .{ .o = op, .args = args } });
-    }
-
-    fn list_edit(self: *@This(), f: *const fn (*OpList, usize) std.mem.Allocator.Error!usize) !void {
-        if (self.cursor_stack.pop()) |c| {
-            const p = self.cursor();
-
-            switch (p.*) {
-                .e_assop => |*assop| {
-                    const idx = c - assop.args.items.ptr;
-
-                    const i = try f(&assop.args, idx);
-
-                    self.cursor_stack.appendAssumeCapacity(&assop.args.items[i]);
-                },
-                else => unreachable,
-            }
-        }
-    }
-
-    fn remove_cursor_node(self: *@This()) !void {
-        const t = struct {
-            fn edit(args: *OpList, idx: usize) std.mem.Allocator.Error!usize {
-                var x = args.orderedRemove(idx);
-                free_children(&x);
-                return if (idx < args.items.len) idx else idx - 1;
-            }
-        };
-
-        try self.list_edit(t.edit);
-
-        // If we have an operation with just one operand, we replace it with the
-        // operand itself.
-
-        if (self.cursor_stack.pop()) |c| {
-            const p = self.cursor();
-
-            switch (p.*) {
-                .e_assop => |*assop| {
-                    if (assop.args.items.len == 1) {
-                        overwrite_expr(p, c.*);
-                        return;
-                    }
-                },
-                else => {},
-            }
-
-            self.cursor_stack.appendAssumeCapacity(c);
-        }
-    }
 };
 
 export fn create_srcprg() ?*anyopaque {
@@ -213,7 +160,7 @@ export fn create_srcprg() ?*anyopaque {
 
     const s = cator.create(Srcprg) catch return null;
     s.tree = x.root;
-    s.cursor_stack = x.stack;
+    s.cursor = x.cursor;
     s.buf = .{};
 
     return @ptrCast(s);
@@ -224,7 +171,14 @@ fn render(srcprg: *Srcprg) !c_interface.frame {
 
     var start: c_int = undefined;
     var end: c_int = undefined;
-    try render_node(srcprg.tree, srcprg.cursor(), AssOp.add, &srcprg.buf, &start, &end);
+    try render_node(
+        srcprg.tree,
+        srcprg.cursor.cursor_pos,
+        AssOp.add,
+        &srcprg.buf,
+        &start,
+        &end,
+    );
 
     const len: c_int = @intCast(srcprg.buf.items.len);
 
@@ -247,32 +201,33 @@ export fn execute(srcprg_opaque: *anyopaque, instr: c_interface.instruction) c_i
     const srcprg: *Srcprg = @ptrCast(@alignCast(srcprg_opaque));
 
     if (instr == c_interface.go_up) {
-        _ = srcprg.cursor_stack.pop();
-    } else if (instr == c_interface.go_left or instr == c_interface.go_right) {
-        srcprg.go_left_or_right(instr) catch std.process.exit(1);
+        srcprg.cursor.perform(.go_up);
+    } else if (instr == c_interface.go_left) {
+        srcprg.cursor.perform(.go_left);
+    } else if (instr == c_interface.go_right) {
+        srcprg.cursor.perform(.go_right);
     } else if (instr == c_interface.go_down) {
-        switch (srcprg.cursor().*) {
-            .e_assop => |op| {
-                std.debug.assert(op.args.items.len != 0);
-                srcprg.cursor_stack.append(cator, &op.args.items[0]) catch std.process.exit(1);
-            },
-            else => {},
-        }
+        srcprg.cursor.perform(.go_down);
     } else if (instr == c_interface.make_into_hole) {
-        const top = srcprg.cursor();
-        overwrite_expr(top, .e_hole);
-    } else if (instr == c_interface.insert_before or instr == c_interface.insert_after) {
-        srcprg.insert_before_or_after(instr) catch std.process.exit(1);
+        srcprg.cursor.perform(.make_into_hole);
+    } else if (instr == c_interface.insert_before) {
+        srcprg.cursor.perform(.insert_before);
+    } else if (instr == c_interface.insert_after) {
+        srcprg.cursor.perform(.insert_after);
     } else if (instr == c_interface.remove_cursor_node) {
-        srcprg.remove_cursor_node() catch std.process.exit(1);
+        srcprg.cursor.perform(.remove_cursor_node);
     }
+
     return render(srcprg) catch std.process.exit(1);
 }
 
 export fn replace_cursor_with_number(srcprg_opaque: *anyopaque, num: c_uint) c_interface.frame {
     const srcprg: *Srcprg = @ptrCast(@alignCast(srcprg_opaque));
 
-    srcprg.replace_cursor_with_number(@intCast(num));
+    const new_expr = cator.create(Expr) catch std.process.exit(1);
+    new_expr.* = .{ .e_const = @intCast(num) };
+
+    srcprg.cursor.perform(.{ .replace_with = .{ .e_const = @intCast(num) } });
 
     return render(srcprg) catch std.process.exit(1);
 }
@@ -280,7 +235,7 @@ export fn replace_cursor_with_number(srcprg_opaque: *anyopaque, num: c_uint) c_i
 export fn replace_cursor_with_addition(srcprg_opaque: *anyopaque) c_interface.frame {
     const srcprg: *Srcprg = @ptrCast(@alignCast(srcprg_opaque));
 
-    srcprg.replace_cursor_with_op(.add) catch std.process.exit(1);
+    srcprg.cursor.perform(.{ .replace_with = new_op(.add) catch std.process.exit(1) });
 
     return render(srcprg) catch std.process.exit(1);
 }
@@ -288,7 +243,7 @@ export fn replace_cursor_with_addition(srcprg_opaque: *anyopaque) c_interface.fr
 export fn replace_cursor_with_multiplication(srcprg_opaque: *anyopaque) c_interface.frame {
     const srcprg: *Srcprg = @ptrCast(@alignCast(srcprg_opaque));
 
-    srcprg.replace_cursor_with_op(.mul) catch std.process.exit(1);
+    srcprg.cursor.perform(.{ .replace_with = new_op(.mul) catch std.process.exit(1) });
 
     return render(srcprg) catch std.process.exit(1);
 }
@@ -304,3 +259,187 @@ fn free_children(node: *Expr) void {
         else => {},
     }
 }
+
+const ThreadCursor = struct {
+    const th = std.Thread;
+
+    mutex: th.Mutex,
+    cond: th.Condition,
+    pending_instruction: ?instruction,
+    root: *Expr,
+    cursor_pos: usize,
+
+    const Self = @This();
+
+    pub const instruction = union(enum) {
+        go_right,
+        go_up,
+        go_left,
+        go_down,
+        insert_before,
+        insert_after,
+        make_into_hole,
+        remove_cursor_node,
+        replace_with: Expr,
+    };
+
+    pub fn perform(self: *Self, instr: instruction) void {
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        self.pending_instruction = instr;
+        self.cond.signal();
+
+        while (true) {
+            self.cond.wait(&self.mutex);
+
+            if (self.pending_instruction == null) {
+                break;
+            }
+        }
+    }
+
+    fn receive(self: *Self) instruction {
+        self.mutex.lock();
+        // We won't unlock the mutex before returning since it's all synchronous.
+
+        while (true) {
+            if (self.pending_instruction) |instr| {
+                return instr;
+            } else {
+                self.cond.wait(&self.mutex);
+            }
+        }
+    }
+
+    fn report_completion(self: *Self, cursor_pos: usize) void {
+        self.pending_instruction = null;
+        self.cursor_pos = cursor_pos;
+        self.cond.signal();
+        self.mutex.unlock();
+    }
+
+    pub fn init(root: *Expr) Self {
+        return Self{
+            .mutex = .{},
+            .cond = .{},
+            .pending_instruction = null,
+            .root = root,
+            .cursor_pos = @intFromPtr(root),
+        };
+    }
+
+    // The different instructions that a call to traverse_dynamically might have
+    // to return to its parent call.
+    const up_instruction = enum {
+        // For the "go up" command.
+        do_nothing,
+        go_left,
+        go_right,
+        insert_before,
+        insert_after,
+        remove_cursor_node,
+    };
+
+    const down_instruction = enum {
+        none,
+        go_down,
+    };
+
+    pub fn start(self: *Self) void {
+        _ = th.spawn(.{}, thread_main, .{self}) catch std.process.exit(1);
+    }
+
+    fn thread_main(self: *Self) void {
+        while (true) {
+            _ = self.traverse_dynamically(self.root, .none) catch std.process.exit(1);
+            self.report_completion(@intFromPtr(self.root));
+        }
+    }
+
+    fn traverse_dynamically(
+        self: *Self,
+        node: *Expr,
+        down_instr: down_instruction,
+    ) !up_instruction {
+        switch (down_instr) {
+            .none => {},
+            .go_down => {
+                self.report_completion(@intFromPtr(node));
+            },
+        }
+
+        while (true) {
+            const instr = self.receive();
+
+            switch (instr) {
+                .go_right => return .go_right,
+                .go_up => return .do_nothing,
+                .go_left => return .go_left,
+                .go_down => {
+                    switch (node.*) {
+                        .e_assop => |*op| {
+                            std.debug.assert(op.args.items.len != 0);
+
+                            var i: usize = 0;
+
+                            while (true) {
+                                const up_instr = try self.traverse_dynamically(&op.args.items[i], .go_down);
+
+                                switch (up_instr) {
+                                    .go_left => {
+                                        if (0 < i) {
+                                            i -= 1;
+                                        }
+                                    },
+                                    .go_right => {
+                                        if (i + 1 < op.args.items.len) {
+                                            i += 1;
+                                        }
+                                    },
+                                    .do_nothing => {
+                                        break;
+                                    },
+                                    .insert_before => {
+                                        try op.args.insert(cator, i, .e_hole);
+                                    },
+                                    .insert_after => {
+                                        i += 1;
+                                        try op.args.insert(cator, i, .e_hole);
+                                    },
+                                    .remove_cursor_node => {
+                                        var x = op.args.orderedRemove(i);
+                                        free_children(&x);
+
+                                        if (op.args.items.len <= i) {
+                                            i -= 1;
+                                        }
+
+                                        if (op.args.items.len == 1) {
+                                            var v = op.args;
+                                            node.* = v.items[0];
+                                            v.clearAndFree(cator);
+                                            break;
+                                        }
+                                    },
+                                }
+                            }
+                        },
+                        else => {},
+                    }
+                },
+                .insert_before => return .insert_before,
+                .insert_after => return .insert_after,
+                .make_into_hole => {
+                    overwrite_expr(node, .e_hole);
+                },
+                .remove_cursor_node => return .remove_cursor_node,
+                .replace_with => |new_node| {
+                    overwrite_expr(node, new_node);
+                },
+            }
+
+            self.report_completion(@intFromPtr(node));
+        }
+    }
+};
