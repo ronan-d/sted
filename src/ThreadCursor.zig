eed2ca32c176cefdfd89a756ab717b14897fdcb7
Ronan Desplanques <ronan.desplanques@gmail.com>
2026-09-15 22:16:12 +0200

    Checkpoint

--- src/ThreadCursor.zig
+++ src/ThreadCursor.zig
@@ -263,6 +263,7 @@ fn mergeMasks(am: AboveMask, node: Tree) Mask {
     ret.getPtr(.insert_inside).* = node.getMask().insert_at and node.childCount() == 0;
     ret.getPtr(.remove).* = am.remove_cursor_node;
     ret.getPtr(.replace).* = !is_replacing_useless;
+    ret.getPtr(.enter_number_input_mode).* = node.getMask().input_number;
 
     return ret;
 }
