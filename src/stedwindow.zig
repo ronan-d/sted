ea9eead1a26ca9f82557527411ceb284c7115f16
Ronan Desplanques <ronan.desplanques@gmail.com>
2026-09-15 22:17:42 +0200

    Checkpoint

--- src/stedwindow.zig
+++ src/stedwindow.zig
@@ -113,7 +113,6 @@ pub const StedWindow = extern struct {
                 const local_module = struct {
                     fn cb(_: *Core, w: *StedWindow) void {
                         Core.modes.number_input_mode.switchToNumberInputMode(w.getTextView());
-                        std.process.exit(0);
                     }
                 };
 
